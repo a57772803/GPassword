a70790a0a0c3737697437fa2e629f3c07d12551a
LeonChen <leonchen1216@gmail.com>
2023-09-09 17:58:23 +0800

    add tailwindcss package and LoginComponent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,7 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
+</script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
-  <HelloWorld/>
-
   <router-view></router-view>
 </template>
 
