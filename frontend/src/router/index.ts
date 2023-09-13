3ab4bc983304902fa562fb7c8f36edaf411049ae
LeonChen <leonchen1216@gmail.com>
2023-09-13 18:55:32 +0800

    添加 dashboard 參考用組件

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory,RouterOptions,Router, RouteRecordRaw } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import Login from '../components/Login.vue'
+import DashBoard from '../components/DashBoard.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: HelloWorld },
-    { path: '/login', component: Login },
+  { path: '/login', component: Login },
+        { path: '/DashBoard', component: DashBoard },
 ]
 const options: RouterOptions = {
  history: createWebHashHistory(),
